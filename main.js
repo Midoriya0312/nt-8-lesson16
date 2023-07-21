5fe9800bae73e03fe36adc542d4cfc55582358cb
Midoriya0312 <oybektojiboyev0312@gmail.com>
2023-07-21 10:15:52 +0500

    lesson16

---
+++ main.js
@@ -0,0 +1,33 @@
+// Homework map
+
+// 1
+
+// const numbers = [65, 44, 12, 4];
+// const newArr = numbers.map(myFunction)
+
+// function myFunction(num) {
+//   return num * 10;
+// }
+
+// 2
+
+// const numbers = [4, 9, 16, 25];
+// const newArr = numbers.map(Math.sqrt)
+
+// 3
+
+// const persons = [
+//     {firstname : "Malcom", lastname: "Reynolds"},
+//     {firstname : "Kaylee", lastname: "Frye"},
+//     {firstname : "Jayne", lastname: "Cobb"}
+//   ];
+  
+//   persons.map(getFullName);
+  
+//   function getFullName(item) {
+//     return [item.firstname,item.lastname].join(" ");
+// }
+
+
+
+
